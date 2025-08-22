e56d0cdb6c4e8ece475389a5148b4aded9bb4d55
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-08-22 18:24:19 +0000

    .

--- shell.c
+++ shell.c
@@ -2,71 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
-char *get_path_from_env(void)
-{
-	int i;
-	char *val;
-
-	for (i = 0; environ[i]; i++)
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-		{
-			val = environ[i] + 5;
-			return val;
-		}
-	}
-	return NULL;
-}
-
 char *find_command(char *cmd)
 {
-	char *path_env, *path_copy, *dir, *full_path;
+	char *path, *dir, *full_path;
+	char *path_copy;
 	int len;
 
-	if (cmd[0] == '/' || cmd[0] == '.')
+	if (strchr(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
 			return strdup(cmd);
 		return NULL;
 	}
 
-	path_env = get_path_from_env();
-	if (!path_env)
+	path = getenv("PATH");
+	if (!path)
 		return NULL;
 
-	path_copy = strdup(path_env);
-	if (!path_copy)
-		return NULL;
-
+	path_copy = strdup(path);
 	dir = strtok(path_copy, ":");
 	while (dir)
 	{
-		len = strlen(dir) + 1 + strlen(cmd) + 1;
+		len = strlen(dir) + strlen(cmd) + 2;
 		full_path = malloc(len);
-		if (!full_path)
-		{
-			free(path_copy);
-			return NULL;
-		}
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, cmd);
-
+		snprintf(full_path, len, "%s/%s", dir, cmd);
 		if (access(full_path, X_OK) == 0)
 		{
 			free(path_copy);
 			return full_path;
 		}
-
 		free(full_path);
 		dir = strtok(NULL, ":");
 	}
-
 	free(path_copy);
 	return NULL;
 }
@@ -74,49 +45,48 @@ char *find_command(char *cmd)
 int main(void)
 {
 	char *line = NULL;
-	size_t cap = 0;
-	ssize_t nread;
-	const char *prompt = "#cisfun$ ";
+	size_t n = 0;
+	ssize_t read;
+	char *argv_child[100];
+	char *token;
+	char *cmd_path;
+	pid_t pid;
+	int status;
+	int i;
 
 	while (1)
 	{
-		char *token;
-		int argc;
-		pid_t pid;
-		int status;
-		char *argv_child[100];
-		char *cmd_path;
-
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, prompt, strlen(prompt));
-
-		nread = getline(&line, &cap, stdin);
-		if (nread == -1)
+		printf(":) ");
+		fflush(stdout);
+		read = getline(&line, &n, stdin);
+		if (read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
+			putchar('\n');
 			break;
 		}
 
-		if (nread > 0 && line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
-		argc = 0;
+		i = 0;
 		token = strtok(line, " \t");
-		while (token != NULL && argc < 99)
+		while (token && i < 99)
 		{
-			argv_child[argc++] = token;
+			argv_child[i] = token;
+			i++;
 			token = strtok(NULL, " \t");
 		}
-		argv_child[argc] = NULL;
-
-		if (argc == 0)
+		argv_child[i] = NULL;
+		if (i == 0)
 			continue;
+
+		if (strcmp(argv_child[0], "exit") == 0)
+			break;
 
 		cmd_path = find_command(argv_child[0]);
 		if (!cmd_path)
 		{
-			fprintf(stderr, "%s: command not found\n", argv_child[0]);
+			fprintf(stderr, "./hsh: 1: %s: not found\n", argv_child[0]);
 			continue;
 		}
 
@@ -139,7 +109,6 @@ int main(void)
 			free(cmd_path);
 		}
 	}
-
 	free(line);
 	return 0;
 }
