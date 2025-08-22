e22ed6e64949fbaf3688fdb1c796024cfc6c7de2
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-08-22 18:48:27 +0000

    .

--- shell.c
+++ shell.c
@@ -2,34 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 
-/* Function to split a line into tokens */
+/* Function to split line into tokens (arguments) */
 char **tokenize(char *line)
 {
+	int bufsize = 64;
+	int position = 0;
 	char **tokens;
 	char *token;
-	int bufsize = 64, i = 0;
 
 	tokens = malloc(bufsize * sizeof(char *));
 	if (!tokens)
 	{
-		perror("malloc");
+		fprintf(stderr, "allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
 	token = strtok(line, " \t\r\n");
 	while (token != NULL)
 	{
-		tokens[i] = token;
-		i++;
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
 		token = strtok(NULL, " \t\r\n");
 	}
-
-	tokens[i] = NULL;
+	tokens[position] = NULL;
 	return tokens;
+}
+
+/* Function to search command in PATH */
+char *find_command(char *command)
+{
+	char *path_env;
+	char *path;
+	char *full_path;
+	char *dir;
+	size_t len;
+
+	path_env = getenv("PATH");
+	if (!path_env)
+		return NULL;
+
+	path = strdup(path_env);
+	if (!path)
+		return NULL;
+
+	dir = strtok(path, ":");
+	while (dir)
+	{
+		len = strlen(dir) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (!full_path)
+		{
+			free(path);
+			return NULL;
+		}
+		snprintf(full_path, len, "%s/%s", dir, command);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path);
+			return full_path;
+		}
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	free(path);
+	return NULL;
 }
 
 /* Execute command */
@@ -37,42 +90,69 @@ void execute(char **args)
 {
 	pid_t pid;
 	int status;
+	char *cmd_path;
 
 	if (args[0] == NULL)
 		return;
 
-	pid = fork();
-	if (pid == 0) /* Child process */
+	if (strchr(args[0], '/'))
 	{
-		execvp(args[0], args);
-		fprintf(stderr, "%s: command not found\n", args[0]);
+		if (access(args[0], X_OK) != 0)
+		{
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			return;
+		}
+		cmd_path = strdup(args[0]);
+	}
+	else
+	{
+		cmd_path = find_command(args[0]);
+		if (!cmd_path)
+		{
+			fprintf(stderr, "%s: not found\n", args[0]);
+			return;
+		}
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd_path, args, NULL);
+		fprintf(stderr, "%s: execution failed\n", args[0]);
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
 	{
 		perror("fork");
 	}
-	else /* Parent process */
+	else
 	{
 		waitpid(pid, &status, 0);
 	}
+
+	free(cmd_path);
 }
 
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	char **args;
 	ssize_t read;
+	char **args;
 
 	while (1)
 	{
 		printf("#cisfun$ ");
+		fflush(stdout);
+
 		read = getline(&line, &len, stdin);
 		if (read == -1)
 		{
 			if (feof(stdin))
+			{
+				printf("\n");
 				break;
+			}
 			continue;
 		}
 
